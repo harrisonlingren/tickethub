0bce3161f7dfb772c769215d9028e67d821983f8
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-22 19:29:44 -0500

    project scaffolding

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="css/materialize.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+
+    <header>
+      <!-- header content here -->
+      <h5 class="white-text">Title here</h5>
+    </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include('includes/header.php'); ?>
+
+
+
+<main>
+  <!-- main content here -->
+
+
+</main>
+
+
+
+<?php include('includes/footer.php'); ?>
