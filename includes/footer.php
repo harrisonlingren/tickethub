618e634ec199431788e94f20777d266e7fa9e4ec
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-29 19:21:45 -0500

    restructure

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,8 @@
       <h5 class="white-text">Footer content here</h5>
     </footer>
 
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/app.js"></script>
-    <script src="../js/materialize.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/materialize.js"></script>
   </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@
 
 <html>
   <head>
-    <link rel="stylesheet" href="../css/materialize.min.css" />
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="css/materialize.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
 
