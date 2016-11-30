56328c24f631959cfe3c9fca93e84e533985d256
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-29 19:19:34 -0500

    restructure

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+
+    <footer>
+      <!-- footer content here -->
+      <h5 class="white-text">Footer content here</h5>
+    </footer>
+
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/app.js"></script>
+    <script src="../js/materialize.js"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php
+  include('error_report.php');
+  include('db_connect.php');
+?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="../css/materialize.min.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+  </head>
+  <body>
+
+    <header>
+      <!-- header content here -->
+      <h5 class="white-text">Title here</h5>
+    </header>
