26620fc005e6f7b8b51f49bdd7ab06a24af66a7a
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-03 22:18:58 -0500

    testing db

--- movies.php
+++ movies.php
@@ -3,55 +3,57 @@
 ?>
 
 <div class="container">
-  <div class="row">
+  <form action="tickets.php">
+    <div class="row">
 
-  <?php
-    $img_base = 'https://image.tmdb.org/t/p/w500';
+    <?php
+      $img_base = 'https://image.tmdb.org/t/p/w500';
 
-    $movies_query = "SELECT movies.id, title, summary, release_date, genre, genre_extra, rating, backdrop_url FROM movies ORDER BY release_date DESC";
-    $exec_q = mysqli_query($dbc, $movies_query);
+      $movies_query = "SELECT movies.id, title, summary, release_date, genre, genre_extra, rating, backdrop_url FROM movies ORDER BY release_date DESC";
+      $exec_q = mysqli_query($dbc, $movies_query);
 
-    $count = 0;
+      $count = 0;
 
-    if($exec_q) {
-      while($movie = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
-        $genres = $movie['genre'];
-        if($movie['genre_extra']) {
-          $genres .= ', ' . $movie['genre_extra'];
-        }
+      if($exec_q) {
+        while($movie = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
+          $genres = $movie['genre'];
+          if($movie['genre_extra']) {
+            $genres .= ', ' . $movie['genre_extra'];
+          }
 
-        if(strlen($movie['summary']) > 150) {
-          $summary = substr($movie['summary'], 0, 147) . '...';
-        } else {
-          $summary = $movie['summary'];
-        }
+          if(strlen($movie['summary']) > 150) {
+            $summary = substr($movie['summary'], 0, 147) . '...';
+          } else {
+            $summary = $movie['summary'];
+          }
 
-        if($count % 3 == 0) {
-          echo '</div><div class="row">' . "\n";
-        }
+          if($count % 3 == 0) {
+            echo '</div><div class="row">' . "\n";
+          }
 
-        echo '<div class="col s12 m6 l4">
-          <div class="card medium">
-            <div class="card-image">
-              <img src="' . $img_base . $movie['backdrop_url'] . '" />
-              <span class="card-title"><h5>' . $movie['title'] . '</h5></span>
+          echo '<div class="col s12 m6 l4">
+            <div class="card medium">
+              <div class="card-image">
+                <img src="' . $img_base . $movie['backdrop_url'] . '" />
+                <span class="card-title"><h5>' . $movie['title'] . '</h5></span>
+              </div>
+              <div class="card-content">
+                <b>' . $movie['rating'] . ' | ' . $genres . '</b>
+                <p>' . $summary . '</p>
+              </div>
+              <div class="card-action">
+                <a onclick="' . "$('#movie_id').val('" . $movie['id'] . "')" . '">Tickets</a>
+              </div>
             </div>
-            <div class="card-content">
-              <b>' . $movie['rating'] . ' | ' . $genres . '</b>
-              <p>' . $summary . '</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Tickets</a>
-            </div>
-          </div>
-        </div>' . "\n";
+          </div>' . "\n";
 
-        $count += 1;
+          $count += 1;
+        }
       }
-    }
-  ?>
-
-  </div>
+    ?>
+    </div>
+    <input type="hidden" value="" name="movie_id" id="movie_id" />
+  </form>
 </div>
 
 <?php
