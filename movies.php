d861d1d4cf7b2f529857d9a5c4a9c09f7dc07e58
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-03 21:39:57 -0500

    testing db

--- movies.php
+++ movies.php
@@ -2,41 +2,49 @@
   include('includes/header.php');
 ?>
 
+<div class="container">
+
+  <?php
+    $img_base = 'https://image.tmdb.org/t/p/w500';
+
+    $movies_query = "SELECT movies.id, title, summary, release_date, genre, genre_extra, rating, backdrop_url FROM movies ORDER BY release_date DESC";
+    $exec_q = mysqli_query($dbc, $movies_query);
+
+    $count = 0;
+
+    if($exec_q) {
+      while($movie = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
+        $count += 1;
+
+        if($count % 3 == 0) {
+          echo '<div class="row">';
+        }
+
+        echo '<div class="col s4>"
+          <div class="card large">
+            <div class="card-image">
+              <img src="' . $img_base . $movie['backdrop_url'] . '" />
+              <span class="card-title">' . $movie['title'] . '</span>
+            </div>
+            <div class="card-content">
+              <h3>' . $movie['rating'] . ' | ' . $movie['genres'] . '</h3>
+              <p>' . $movie['summary'] . '</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Tickets</a>
+            </div>
+          </div>
+        </div>';
+
+        if($count % 3 == 0) {
+          echo '</div>';
+        }
+      }
+    }
+  ?>
+
+</div>
 
 <?php
-  $img_base = 'https://image.tmdb.org/t/p/w500';
-
-  $movies_query = "SELECT movies.id, title, summary, release_date, genre, genre_extra, rating, backdrop_url FROM movies ORDER BY release_date DESC";
-  $exec_q = mysqli_query($dbc, $movies_query);
-
-  if($exec_q) {
-    while($movie = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
-      echo '<div class="card large">
-        <div class="card-image">
-          <img src="' . $img_base . $movie['backdrop_url'] . '" />
-          <span class="card-title">' . $movie['title'] . '</span>
-        </div>
-        <div class="card-content">
-          <h3>' . $movie['rating'] . ' | ' . $movie['genres'] . '</h3>
-          <p>' . $movie['summary'] . '</p>
-        </div>
-        <div class="card-action">
-          <a href="#">Tickets</a>
-        </div>
-      </div>';
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
   include('includes/footer.php');
 ?>
