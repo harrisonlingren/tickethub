aaf50b17652d19ba53df82a37773b4d5d7a17b70
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-04 00:33:16 -0500

    list showtimes template

--- tickets.php
+++ tickets.php
@@ -11,30 +11,37 @@
   }
 ?>
 
-<div class="collection with-header">
-  <li class="collection-header"><h4>Available Showtimes</h4></li>
-  <?php
-    // if an ID was posted through, list showtimes for that movie.
-    // otherwise, list all showtimes by date
-    $times_query = "SELECT showings.date, showings.time, theater_id, available_seats FROM showings";
+<div class="container">
+  <ul class="collection with-header">
+    <li class="collection-header"><h4>Available Showtimes</h4></li>
+    <?php
+      // if an ID was posted through, list showtimes for that movie.
+      // otherwise, list all showtimes by date
+      $times_query = "SELECT showings.date, showings.time, theater_id, available_seats FROM showings";
 
-    if($flag) {
-       $times_query .= "WHERE movie_id = $movie";
-    }
-
-    $exec_q = mysqli_query($dbc, $times_query);
-    if($exec_q) {
-      while($time = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
-        echo '<li class="collection-item">
-          <span class="title">' . $time['time'] . '</span>
-          <p>' . $time['date'] . ', ' . $time['date'] . '<br />
-          Open seats: ' . $time['available_seats'] . '
-          </p>
-          <a href="#" class="secondary-content">
-            <i class="material-icons">keyboard arrow right</i>
-          </a>
-        </li>' . "\n";
+      if($flag) {
+         $times_query .= "WHERE movie_id = $movie";
       }
-    }
-  ?>
+
+      $exec_q = mysqli_query($dbc, $times_query);
+      if($exec_q) {
+        while($time = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
+          echo '<li class="collection-item">
+            <span class="title">' . $time['time'] . '</span>
+            <p>' . $time['date'] . ', ' . $time['date'] . '<br />
+            Open seats: ' . $time['available_seats'] . '
+            </p>
+            <a href="#" class="secondary-content">
+              <i class="material-icons">keyboard arrow right</i>
+            </a>
+          </li>' . "\n";
+        }
+      }
+    ?>
+  </div>
+
 </div>
+
+<?php
+  include('../includes/footer.php');
+?>
