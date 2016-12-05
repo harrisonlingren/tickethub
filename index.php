701d54408882ab21b5ac767d141cf787097331f0
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-04 19:14:07 -0500

    finally starting the index page...

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
   include('includes/header.php');
 ?>
 
-<?php
-  include('movies.php');
-?>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="card-panel">
+          <h3>Now Playing</h3>
+        </div>
+      </div>
+      <div class="col s12 m4">
+        <div class="card-panel">
+          <h3>Showtimes</h3>
+        </div>
+      </div>
+      <div class="col s12 m4">
+        <div class="card-panel">
+          <h3>About</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
 
 <?php
   include('includes/footer.php');
