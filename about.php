aa82a77c1d3df722ea239e2d2e3e88495ee9e97f
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 13:30:55 -0500

    about page init

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php include('includes/header.php'); ?>
+
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <h3>About Us</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12 m6 l4">
+        <div class="card-panel">
+          About us stuff here!
+        </div>
+        <div class="card-panel">
+          Even more!
+        </div>
+        <div class="card-panel">
+          And more yet!
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+
+
+<?php include('includes/footer.php'); ?>
