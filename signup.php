faf346e6edd98a058dc4c09adcb04fb3045cde21
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 17:19:09 -0500

    signup form

--- signup.php
+++ signup.php
@@ -15,32 +15,26 @@ if ($logged_in) {
     <div class="row">
       <form class="col s12" action="signup.php" method="POST">
         <div class="row">
-          <div class="col s2">
-            <i class="material-icons large">person</i>
-          </div>
-          <div class="input-field col s5">
+          <div class="input-field col s6 m10 l8 offset-m1 offset-l2">
+            <i class="material-icons">person</i>
             <input id="firstname" name="firstname" type="text" class="validate">
             <label for="firstname">First Name</label>
-          </div>
-          <div class="input-field col s5">
             <input id="lastname" name="lastname" type="text" class="validate">
             <label for="lastname">Last Name</label>
           </div>
         </div>
         <div class="row">
-          <div class="col s2">
-            <i class="large material-icons">email</i>
-          </div>
-          <div class="input-field col s10">
+          <div class="input-field col s12 m10 l8 offset-m1 offset-l2">
+            <i class="material-icons">email</i>
             <input id="email" type="email" class="validate">
             <label for="email">Email</label>
           </div>
         </div>
         <div class="row">
           <div class="col s2">
-            <i class="large material-icons">lock</i>
+            <i class="material-icons">lock</i>
           </div>
-          <div class="input-field col s10">
+          <div class="input-field col s12 m10 l8 offset-m1 offset-l2">
             <input id="password" type="password" class="validate">
             <label for="password">Password</label>
           </div>
