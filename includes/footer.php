8c45f63c27f4e1cbb5336aba7a50289c4c4d07ce
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 16:47:14 -0500

    signup page and footer

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
                   <li><a class="grey-text text-lighten-3" href="showtimes.php">Showtimes</a></li>
                   <li><a class="grey-text text-lighten-3" href="about.php">About</a></li>
                 </ul>
-                <ul class="right">
+                <ul class="footer-right">
                   <li><a class="grey-text text-lighten-3" href="account.php">My Account</a></li>
                   <li><a class="grey-text text-lighten-3" href="signup.php">Sign Up</a></li>
                 </ul>
