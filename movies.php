4803b202749365fca22c7dd8ce8122b0db39633c
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 14:32:56 -0500

    search button

--- movies.php
+++ movies.php
@@ -89,10 +89,10 @@
 
 <div id="searchModal" class="modal bottom-sheet">
   <div class="modal-content">
-    <h4>Enter a movie title to search!</h4>
+    <h4><i class="material-icons">search</i> Enter a movie title to search!</h4>
     <form method="GET" action="movies.php">
      <div class="input-field">
-       <input id="search" name="q" type="search" required>
+       <input id="search" name="q" type="search" placeholder="Search movies..." required>
        <i class="material-icons">close</i>
      </div>
    </form>
