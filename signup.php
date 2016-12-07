5fe72822293091e1deb21b98631c29e3ccf72705
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 16:54:48 -0500

    account & signup pages

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 
 if ($logged_in) {
   echo '<h3>Already logged in! Redirecting...</h3>';
-  echo "<script>window.location = '//blue.butler.edu/~hlingren/CME419/tickethub/account.php';</script>"
+  echo "<script>window.location = '//blue.butler.edu/~hlingren/CME419/tickethub/account.php';</script>";
 }
 
 ?>
