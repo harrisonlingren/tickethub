9b426cff1f2ddc3d7f2d7f340dd966f119704972
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 18:13:45 -0500

    init info and orders pages

---
+++ my-info.php
@@ -0,0 +1,17 @@
+<?php
+  include('includes/header.php');
+  include('includes/check_account.php');
+?>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <h3>Main content here!</h3>
+      </div>
+    </div>
+  </div>
+</main>
+
+
+<?php include('includes/footer.php'); ?>
--- orders.php
+++ orders.php
@@ -1,5 +1,17 @@
-<?php include('includes/header.php'); ?>
+<?php
+  include('includes/header.php');
+  include('includes/check_account.php');
+?>
 
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <h3>Main content here!</h3>
+      </div>
+    </div>
+  </div>
+</main>
 
 
 <?php include('includes/footer.php'); ?>
