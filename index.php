6d59b9769825ee86629e080b45373dca7c1b3503
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-06 22:45:42 -0500

    icons on home page

--- index.php
+++ index.php
@@ -12,17 +12,20 @@
     <div class="row">
       <div class="col s12 m4">
         <div class="card-panel">
+          <i class="material-icons large">movie</i>
           <a href="movies.php"><h4>Now Playing</h4></a>
         </div>
       </div>
       <div class="col s12 m4">
         <div class="card-panel">
+          <i class="material-icons large">watch_later</i>
           <a href="showtimes.php"><h4>Showtimes</h4></a>
         </div>
       </div>
       <div class="col s12 m4">
         <div class="card-panel">
-          <a href="showtimes.php"><h4>About</h4></a>
+          <i class="material-icons large">info</i>
+          <a href="showtimes.php"><h4>About Us</h4></a>
         </div>
       </div>
     </div>
