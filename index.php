50f1b94236fcf9326a7c72b3040c03203a86709b
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-06 22:56:31 -0500

    width spacing

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
       </div>
     </div>
     <div class="row action-call">
-      <div class="col s12 m4">
+      <div class="col s12 l4">
         <div class="card-panel">
           <i class="material-icons large">movie</i>
           <a href="movies.php"><h4>Now Playing</h4></a>
         </div>
       </div>
-      <div class="col s12 m4">
+      <div class="col s12 l4">
         <div class="card-panel">
           <i class="material-icons large">watch_later</i>
           <a href="showtimes.php"><h4>Showtimes</h4></a>
         </div>
       </div>
-      <div class="col s12 m4">
+      <div class="col s12 l4">
         <div class="card-panel">
           <i class="material-icons large">info</i>
           <a href="showtimes.php"><h4>About Us</h4></a>
