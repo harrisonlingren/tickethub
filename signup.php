1817c8678cb82827988d8915b04d37283df337ea
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 17:01:41 -0500

    signup form

--- signup.php
+++ signup.php
@@ -10,6 +10,53 @@ if ($logged_in) {
 
 
 <main>
+  <div class="row">
+    <form class="col s12" action="signup.php" method="POST">
+      <div class="row">
+        <div class="col s2">
+          <i class="material-icons large">person</i>
+        </div>
+        <div class="input-field col s5">
+          <input placeholder="Placeholder" id="firstname" name="firstname" type="text" class="validate">
+          <label for="firstname">First Name</label>
+        </div>
+        <div class="input-field col s5">
+          <input id="lastname" name="lastname" type="text" class="validate">
+          <label for="lastname">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s2">
+          <i class="large material-icons">email</i>
+        </div>
+        <div class="input-field col s10">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s2">
+          <i class="large material-icons">lock</i>
+        </div>
+        <div class="input-field col s10">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          This is an inline input field:
+          <div class="input-field inline">
+            <input id="email" type="email" class="validate">
+            <label for="email" data-error="Enter a valid email address">Email</label>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+
+
+
 
 </main>
 
