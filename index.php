6113298aabcdda2ed4710dd9d3d4cbf281246224
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-06 22:49:36 -0500

    icon colors

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
         <h3>Home</h3>
       </div>
     </div>
-    <div class="row">
+    <div class="row primary-color">
       <div class="col s12 m4">
         <div class="card-panel">
-          <i class="material-icons large">movie</i>
+          <i class="material-icons large primary-color">movie</i>
           <a href="movies.php"><h4>Now Playing</h4></a>
         </div>
       </div>
       <div class="col s12 m4">
         <div class="card-panel">
-          <i class="material-icons large">watch_later</i>
+          <i class="material-icons large primary-color">watch_later</i>
           <a href="showtimes.php"><h4>Showtimes</h4></a>
         </div>
       </div>
       <div class="col s12 m4">
         <div class="card-panel">
-          <i class="material-icons large">info</i>
+          <i class="material-icons large primary-color">info</i>
           <a href="showtimes.php"><h4>About Us</h4></a>
         </div>
       </div>
