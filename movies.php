4c54d36e222e585cfa9469a745745232aab85fed
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 14:31:59 -0500

    search button

--- movies.php
+++ movies.php
@@ -93,7 +93,6 @@
     <form method="GET" action="movies.php">
      <div class="input-field">
        <input id="search" name="q" type="search" required>
-       <label for="search"><i class="material-icons">search</i></label>
        <i class="material-icons">close</i>
      </div>
    </form>
