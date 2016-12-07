fbaccc2d8b8087ab7e5dd6989f74a259db27dc79
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 10:34:31 -0500

    icon color

--- index.php
+++ index.php
@@ -12,20 +12,26 @@
     <div class="row action-call">
       <div class="col s12 l4">
         <div class="card-panel">
-          <i class="material-icons large">movie</i>
-          <a href="movies.php"><h4>Now Playing</h4></a>
+          <a href="movies.php">
+            <i class="material-icons large">movie</i>
+            <h4 class="grey-text darken-4">Now Playing</h4>
+          </a>
         </div>
       </div>
       <div class="col s12 l4">
         <div class="card-panel">
-          <i class="material-icons large">watch_later</i>
-          <a href="showtimes.php"><h4>Showtimes</h4></a>
+          <a href="showtimes.php">
+            <i class="material-icons large">watch_later</i>
+            <h4 class="grey-text darken-4">Showtimes</h4>
+          </a>
         </div>
       </div>
       <div class="col s12 l4">
         <div class="card-panel">
-          <i class="material-icons large">info</i>
-          <a href="showtimes.php"><h4>About Us</h4></a>
+          <a href="about.php">
+            <i class="material-icons large">info</i>
+            <h4 class="grey-text darken-4">About Us</h4>
+          </a>
         </div>
       </div>
     </div>
