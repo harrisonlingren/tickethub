5444b84e9574daa04ad341f0b21a177591c77601
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 17:58:47 -0500

    fix login redirect

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php include('includes/header.php');
   function CheckLoginInDB($email, $password, $dbc) {
     $pwdmd5 = md5($password);
+    echo $pwdmd5;
     $q = "SELECT id, email, password FROM users
           WHERE email='$email' and password='$pwdmd5'";
 
