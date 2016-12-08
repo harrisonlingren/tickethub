20746bc6d909959981e117dd6e737cb0530e2067
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-08 01:06:40 -0500

    rename my-info, start prep for editing orders

--- orders.php
+++ orders.php
@@ -38,7 +38,7 @@
               <div class="collapsible-header"><i class="material-icons">receipt</i>' . $order_title . '</div>
               <div class="collapsible-body">
                 <p>' . $order_desc . '</p>
-                <a class="collapsible-action" onclick="edit_order.php?id=' . $order['id'] . '">Edit Order</a>
+                <a class="collapsible-action" href="edit_order.php?id=' . $order['id'] . '">Edit Order</a>
               </div>
             </li>';
           }
