1030d4274f5b3d56d795e1c5365673f89803944b
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 22:21:14 -0500

    done for the night. init orders pages

--- orders.php
+++ orders.php
@@ -1,4 +1,5 @@
 <?php
+  // this file is for iterating the recent orders (pagination...?)
   include('includes/header.php');
   include('includes/check_account.php');
 ?>
@@ -7,7 +8,7 @@
   <div class="container">
     <div class="row">
       <div class="col s12">
-        <h3>Main content here!</h3>
+        <h3>Recent Orders</h3>
       </div>
     </div>
   </div>
