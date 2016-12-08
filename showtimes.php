ff816ad11f3d5a24986a0ab339c351393a6f7158
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-08 16:46:26 -0500

    refactor showtimes to save to session

--- showtimes.php
+++ showtimes.php
@@ -18,17 +18,20 @@
     <?php
       // initialize array of upcoming dates
       $dates = array();
-      $dates_query = "SELECT DISTINCT showings.date FROM showings ORDER BY showings.date ASC";
+      $dates_query = "SELECT DISTINCT showings.date, showings.time FROM showings ORDER BY showings.date ASC";
       $exec_q = mysqli_query($dbc, $dates_query);
       if ($exec_q) {
         while ($date = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
-          array_push($dates, $date['date']);
+          array_push(
+            $dates,
+            date('', strtotime($date['date'] . ' ' . $date['time']))
+          );
         }
       }
-      // build tabs to navigate between days
-    ?>
 
-    <div class="row">
+
+    // build tabs to navigate between days
+    ?><div class="row">
       <div class="col s12">
         <ul class="tabs tabs-fixed-width">
           <?php
@@ -45,48 +48,65 @@
     <?php
       // if an ID was posted through, list showtimes for that movie for selected date.
       // otherwise, get showtimes for the next 7 days sorted by time
-      for ($i=1; $i<=7; $i++) {
-        $loop_date = $dates[$i-1];
-        $times_query = "SELECT showings.id, movie_id, showings.time, theater_id, available_seats FROM showings WHERE showings.date = '$loop_date'";
-        if($flag) {
-           $times_query .= " AND movie_id = $movie";
+      $date1 = $dates[0]; $date2 = $dates[6];
+
+      $times_query = "SELECT showings.id, movie_id, showings.time, theater_id,
+      available_seats FROM showings
+      WHERE showings.date between $date1 and $date2";
+
+      if($flag) {
+         $times_query .= " AND movie_id = $movie";
+      }
+      $times_query .= "  ORDER BY showings.time ASC";
+
+      echo $times_query;
+
+      $results = array();
+      $exec_q = mysqli_query($dbc, $times_query);
+      if($exec_q) {
+
+        // init results array with results
+        while($time = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
+          array_push($results, $time);
         }
-        $times_query .= "  ORDER BY showings.time ASC";
 
-        // echo $times_query;
+        // iterate through cached rows, add showtime array for each movie and add showings into each movie array
+        $movies = array();
+        foreach ($results as $time) {
+          $mov = $time['movie_id'];
+          $dat = $time['date'];
+          $tim = $time['time'];
+          $formatted_time = $dat . ' ' . $tim;
+          $the = $time['theater_id'];
+          $sea = $time['available_seats'];
+          $showID = $time['id'];
 
-        $results = array();
-        $exec_q = mysqli_query($dbc, $times_query);
-        if($exec_q) {
-          // load results to memory
-          while($time = mysqli_fetch_array($exec_q, MYSQLI_ASSOC)) {
-            array_push($results, $time);
+          if( !array_key_exists($mov, $movies)) {
+            $movies[$mov] = array();
+          }
+          array_push($movies[$mov], new Showtime($formatted_time, $the, $sea, $showID));
+        } print_r($movies);
+
+        // build arrays for each set of showings and save to session
+        $dailyshowings = array();
+        if (!isset($_SESSION['showings'])) {
+          $_SESSION['showings'] = array();
+        }
+
+        for ($i=1; $i<=7; $i++) {
+          $dailyshowings[$i] = array_filter($movies, function($showing) {
+            return ($showing['date'] == $dates[$i-1]);
+          });
+
+          if (!isset($_SESSION['showings'][$i])) {
+            $_SESSION['showings'][$i] = array();
           }
 
-          echo '<div id="day' . $i . '">';
+          $_SESSION['showings'][$i] = $dailyshowings[$i];
+        }
 
-          // iterate through cached rows, add array for each movie and add times to movie arrays
-          $movies = array();
-          foreach ($results as $time) {
-            // $curr_movie = $time['movie_id'];
-
-            $mov = $time['movie_id'];
-            $dat = $loop_date;
-            $tim = $time['time'];
-            $formatted_time = $dat . ' ' . $tim;
-            $the = $time['theater_id'];
-            $sea = $time['available_seats'];
-            $showID = $time['id'];
-
-            if( !array_key_exists($mov, $movies)) {
-              $movies[$mov] = array();
-            }
-            array_push($movies[$mov], new Showtime($formatted_time, $the, $sea, $showID));
-            //print_r($movies);
-          }
-
-          // spit out results for each movie
-          foreach ($movies as $title => $showing) {
+        for ($i=1; $i<=7; $i++) {
+          foreach ($dailyshowings[$i] as $title => $showing) {
             echo '
 
             <h4>' . $MOVIE_TITLES[$title] . '</h4>
@@ -107,14 +127,14 @@
                 </div>
               </li>';
             }
-
             echo '
-            </ul>';
+            </ul>
+          </div>';
           }
-          echo '</div>';
-        } else {
-          echo "No showtimes found!";
         }
+
+      } else {
+        echo "No showtimes found!";
       }
     ?>
   </div>
