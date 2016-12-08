d220ebc6b5d2d7734f6326ba9eb53fb5c061bc2d
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-08 01:08:54 -0500

    restructure order forms

--- order.php
+++ order.php
@@ -1,1 +1,22 @@
-// this file is for creating a new order
+<?php
+  // this file is for iterating the recent orders (pagination...?)
+  include('includes/header.php');
+  include('includes/check_account.php');
+?>
+
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <h3>Recent Orders</h3>
+      </div>
+    </div>
+
+
+
+  </div>
+</main>
+
+
+<?php include('includes/footer.php'); ?>
